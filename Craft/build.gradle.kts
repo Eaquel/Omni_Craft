0ade77a0d57a1877176183f4d89e9142525222cb
Eaquel <shakeofangel@gmail.com>
2026-08-17 01:16:13 +0300

    feat: AAA seviye C++26 motor, GLES 3.2, Turkce HUD ve Documents/Craft_Log altyapisi

--- Craft/build.gradle.kts
+++ Craft/build.gradle.kts
@@ -1,13 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
-    }
-}
-
 plugins {
     id("com.android.application")
 }
